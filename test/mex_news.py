d81cff47f014d73a1e033fd73b91903c6294b5d8
Anne Rogers <amr@cs.uchicago.edu>
2017-02-20 12:26:58 -0600

    Changes

--- test/mex_news.py
+++ test/mex_news.py
@@ -7,11 +7,27 @@ import re
 from io import StringIO
 import newspaper
 from newspaper import Article
+import random
+from random import randint
+from time import sleep
+import pandas as pd
+from newspaper.configuration import Configuration
 
 
 main_url = "http://www.jornada.unam.mx/ultimas"
 
-
+user_agents = ["Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0",
+             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0",
+             "Mozilla/5.0 (Windows NT 6.2; rv:22.0) Gecko/20130405 Firefox/23.0",
+             "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20130406 Firefox/23.0",
+             "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:23.0) Gecko/20131011 Firefox/23.0",
+             "Mozilla/5.0 (Windows NT 6.2; rv:22.0) Gecko/20130405 Firefox/22.0",
+             "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0",
+             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:25.0) Gecko/20100101 Firefox/25.0",
+             "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0",
+             "Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0",
+             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0",
+             "Mozilla/5.0 (Windows NT 6.2; rv:22.0) Gecko/20130405 Firefox/23.0"]
 
 def get_sections(main_url):
     '''
@@ -71,9 +87,13 @@ def get_info(dictionary):
     for key, item in dictionary.items():
         count = 0
         for i in item:
+            sleep(randint(5,15))
+            #config = Configuration()
+            #config.browser_user_agent = random.choice(user_agents)
             article = Article(i, language = 'es')
-            article.download()
-            article.parse()
+            
+            #article.download()
+            article.build()
             count = count + 1
             title = article.title
             print(title, key, count)
