3d9014300ad7699b2f03818ede6fe1f08dbb472b
Aycrazy <andrewyaspan@gmail.com>
2017-03-01 08:20:27 -0600

    updated heroku and django

--- create_archive_search.py
+++ create_archive_search.py
@@ -10,6 +10,7 @@ from newspaper import Article
 import pandas as pd
 import csv
 import numpy as np
+import queue
 
 ref1 = 'http://pqasb.pqarchiver.com/latimes/results.html?st=advanced&QryTxt=White+House&type=current&sortby=CHRON&datetype=0&frommonth=01&fromday=01&fromyear=1985&tomonth=02&today=08&toyear=2017&By=&Title=&at_curr=ALL&Sect=ALL'
 
@@ -29,19 +30,41 @@ def get_input(source, text, from_month, from_day, from_year, to_month, to_day, t
     return archive_url
 
 
-def get_articles(archive_url, tag_type, class_type):
+def get_archive_articles(archive_url, tag_type, class_type):
     '''
     Find all the articles in the newspaper
     Inputs: main url 
     Returns: A list with the links for all the sections
     '''
     pm = urllib3.PoolManager()
-    html = pm.urlopen(url= main_url, method="GET").data
+    html = pm.urlopen(url= archive_url, method="GET").data
     soup = bs4.BeautifulSoup(html,'lxml')
 
-    tag_list = soup.find_all('td', class_= top)
+    tag_list = soup.find_all('td', class_= 'top')
 
-    return [tag.a['href'] for tag in tag_list]
+    return soup, [tag.a['href'] for tag in tag_list]
+
+def get_next_page(archive_url):
+
+    catalog = queue.Queue()
+    pm = urllib3.PoolManager()
+    html = pm.urlopen(url= archive_url, method="GET").data
+    soup = bs4.BeautifulSoup(html,'lxml')
+    print(soup)
+    a_tag = soup.find_all('a')
+    print(a_tag[0])
+    span_tag = soup.find_all('span', class_='resultsheader')
+    print(span_tag)
+    pre = 'http://pqasb.pqarchiver.com/latimes/'
+
+    soup_section = bs4.BeautifulSoup(span_tag[0],lxml)
+    next_pages = soup_section.find_all('a',class_='resultsheader')
+    for np in next_pages:
+        whole = pre+np
+        catalog.add(whole)
+
+    return catalog
+
 
  #div, class_='resultpgs' ,a, href   
 
@@ -58,3 +81,4 @@ def get_user_agent():
 
     return stacked.sample(1).reset_index()[0][0].strip(' ')
 
+    return d
