72afe50be4fc95579469f191c2391e8e5d0770e3
Anne Rogers <amr@cs.uchicago.edu>
2017-03-07 15:30:04 -0600

    Add some plots

--- python-getting-started/scripts/analysis.py
+++ python-getting-started/scripts/analysis.py
@@ -11,10 +11,12 @@ while startdate < date(int("2010"), int("04"), int("10")):
     dt.append(startdate)
     startdate += timedelta(days=1)
 
-scores = [np.random.normal(0, 1) for i in range(len(dt))]
+scores_text = [np.random.normal(0, 1) for i in range(len(dt))]
+scores_title = [np.random.normal(0, 1) for i in range(len(dt))]
 findata = [random.uniform(100, 300) for i in range(len(dt))]
 
-d = {'scores': pd.Series(scores),
+d = {'scores_text': pd.Series(scores_text),
+     'scores_title': pd.Series(scores_title),
      'findata': pd.Series(findata),
      'time': pd.Series(dt)}
 
@@ -31,44 +33,115 @@ df.describe()
  
 # Correlation for the time period
 
-df.scores.corr(df.findata)
+df.scores_text.corr(df.findata)
 
+df.scores_title.corr(df.findata)
 
 # Scatter
 
-def scatter_plot(data):
+def scatter_plot(data, save_to = None):
     '''
+    Computes the scatter plot for sentiment scores_text
+    and financial data for a specified time period
+    Inputs:
+            Pandas DataFrame
+            Filename, default value None
+    Returns:
+            It can only show the plot or save a 
+            file with the plot
     '''
     colors = np.random.rand(len(dt))
-    area = np.pi * (15 * np.random.rand(len(dt)))**2  # 0 to 15 point radii
-    plt.figure()
-    plt.scatter(data.findata, data.scores, s=area, c=colors, alpha=0.5)
+    area = np.pi * (15 * np.array([0.6]*len(dt)))**2  # 0 to 15 point radii
+    f = plt.figure()
+    plt.scatter(data.findata, data.scores_text, s=area, c=colors, alpha=0.5)
     plt.grid(True)
-    return plt.show()
+
+    if save_to == None:
+
+        plt.show()
+
+    else:
+        f.savefig(save_to)
+
+
+# Text vs. title nltk scores 
+
+def scatter_plot_comparison(data, save_to = None):
+    '''
+    Computes the scatter plot for sentiment scores_text
+    and financial data for a specified time period
+    Inputs:
+            Pandas DataFrame
+            Filename, default value None
+    Returns:
+            It can only show the plot or save a 
+            file with the plot
+    '''
+    colors = np.random.rand(len(dt))
+    area = np.pi * (15 * np.array([0.6]*len(dt)))**2  # 0 to 15 point radii
+    plt.scatter(data.scores_title, data.scores_text, s=area, c=colors, alpha=0.5)
+
+    x = np.array(data.scores_text)
+    y = np.array(data.scores_title)
+
+    m, b = np.polyfit(x, y, 1)
+    plt.plot(x, m*x + b, '-')
+    plt.grid(True)
+
+    if save_to == None:
+
+        plt.show()
+
+    else:
+        plt.savefig(save_to)
+
+
 
 
 # Histograms
 
-def histo_plot(data):
+def histo_plot(data, save_to = None):
     '''
+    Computes the histograms for sentiment scores_text
+    and financial data for a specified time period
+    Inputs:
+            Pandas DataFrame
+            Filename, default value None
+    Returns:
+            It can only show the plot or save a 
+            file with the plot
     '''
-    plt.figure()
+    data.hist(layout=(1,2)) 
 
-    return plt.show()
+    if save_to == None:
+
+        plt.show()
+
+    else:
+        plt.savefig(save_to)
 
 
 
 # Time Series
 
-def time_series(data):
+def time_series(data, save_to = None):
     '''
+    Plots sentiment scores_text and financial data for
+    a specified time period
+    
+    Inputs:
+            Pandas DataFrame
+            Filename, default value None
+    Returns:
+            It can only show the plot or save a 
+            file with the plot
     '''
 
-    fig, ax = plt.subplots()
+    f, ax = plt.subplots()
 
     with pd.plot_params.use('x_compat', True):
 
-        data.scores.plot(color = 'b')
+        data.scores_text.plot(color = 'b')
 
         data.findata.plot(secondary_y = True, style='g')
 
@@ -78,4 +151,9 @@ def time_series(data):
    
         plt.grid()
 
-    return plt.show()
+    if save_to == None:
+
+        plt.show()
+
+    else:
+        f.savefig(save_to)
