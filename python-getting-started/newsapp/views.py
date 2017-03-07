ae6a096e54472a9764f1dd965b66535996c59501
Aycrazy <andrewyaspan@gmail.com>
2017-03-07 01:22:57 -0600

    sleep

--- python-getting-started/newsapp/views.py
+++ python-getting-started/newsapp/views.py
@@ -12,40 +12,41 @@ from scripts.forms import UserInput
 # Create your views here.
 
 def search_news(request):
-    return render(request,'templates/search.html')
+    return render(request,'search.html')
 
 def results(request):
 
     # If this is a POST request then process the Form data
     if request.method == 'POST':
+        if form.is_valid():
 
-        # Create a form instance and populate it with data from the request (binding):
-        form = UserInput(request.POST)
+            # Create a form instance and populate it with data from the request (binding):
+            form = UserInput(request.POST, initial= data)
 
-        # Check if the form is valid:
+            # Check if the form is valid:
 
-        if UserInput.stock_or_currency == 'stock':
-            ticker = form.stock_choice()
-            user_dict = get_historical_stock(form.start_date,form.end_date, ticker)
+            if UserInput.stock_or_currency == 'stock':
+                ticker = form.stock_choice()
+                user_dict = get_historical_stock(form.start_date,form.end_date, ticker)
 
-        #will need to filter these based on days there are articles
-        
+            #will need to filter these based on days there are articles
+            
 
-        if form.home == 'United States':
-            paper = 'ProPublica'
-        else:
-            paper = 'Jornada'
+            if form.home == 'United States':
+                paper = 'ProPublica'
+            else:
+                paper = 'Jornada'
 
-        articles = Article.objects.filter(title__contains=form.keyword,pub_date__range=(form.start_date,form.end_date),source = paper)
-        
-        dates = []
-        nltk_scores = []
+            articles = Article.objects.filter(title__contains=form.keyword,pub_date__range=(form.start_date,form.end_date),source = paper)
+            
+            dates = []
+            nltk_scores = []
 
-        for article in articles:
-            dates.append(article.pub_date)
-            nltk_scores.append(article.nltk_score)
+            for article in articles:
+                dates.append(article.pub_date)
+                nltk_scores.append(article.nltk_score)
 
-        c_or_s_list = [v for k,v in user_dict.items() if k in dates]
+            c_or_s_list = [v for k,v in user_dict.items() if k in dates]
 
 
         # 
@@ -62,13 +63,13 @@ def results(request):
             #if there are multiple articles for a keyword for that day -average the sentiment
             #else just use the sentiment for the one article
             #make a list of dates
-    return render(request,'results.html')
+    return render(request,'results.html',{'form': form})
 
 def index(request):
     #r = requests.get('http://httpbin.org/status/418')
     #print (r.text)
 
-    return render(request,'index.html')
+    return render(request,'index.html',{'form': form})
 
 def db(request):
 
