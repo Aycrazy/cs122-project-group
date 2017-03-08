257e1afd59c30bab9ae0878f1c57270cf7480aaa
Aycrazy <andrewyaspan@gmail.com>
2017-03-07 18:58:09 -0600

    pushing migration

--- python-getting-started/newsapp/admin.py
+++ python-getting-started/newsapp/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from newsapp.models import Article #, ArticlesManager
+from newsapp.models import Article, Ticker, Currency
 import csv
 # Register your models here.
 
 #admin.site.register(ArticlesManager)
 admin.site.register(Article)
-
+admin.site.register(Ticker)
+admin.site.register(Currency)
 
 #Find the file -- python any csv in the file path 
 #Figure out exactly where this is to run
